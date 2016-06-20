8ba0a0c7aebd3b5df3ee1eab76406a4484a2463e
Twist-tracer <twist.tracer@gmail.com>
2016-06-20 11:59:28 +0300

    Init

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="author" content="Twist Tracer" />
+    <title>Консоль</title>
+
+    <link rel="stylesheet" type="text/css" href="styles/main.css" />
+    <!--[if lte IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+</body>
+</html>
---
+++ new.php
@@ -0,0 +1,22 @@
+<?php
+$data['request']['unsorted'] = array(
+    'category' => 'sip',
+    'add' => array(
+                'contacts' => array(
+                    array(
+                        'name'          => 'Phone number: 89114248742',
+                        'custom_fields' => array(
+                            array(
+                                'id'     => '1275774',
+                                'values' => array(
+                                    array(
+                                        'enum'  => 2930151,
+                                        'value' => '89114248742',
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+    );
