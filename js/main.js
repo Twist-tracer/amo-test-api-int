0828379e922a99e95f115165b36115ab469b56a0
Twist-tracer <twist.tracer@gmail.com>
2016-06-21 17:08:18 +0300

    add ajax pre-loader

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $("document").ready(function() {
         $.ajax({
             type: "POST",
             url: "index.php",
+            beforeSend: function() {
+                $('#send-request')
+                    .html("<img src='images/ajax-loader.gif' alt='Отправка...' title='Отправка...' />")
+                    .attr("class", "btn btn-default disabled");
+            },
             data: "query=send_request",
             success: function(response){
                 response = JSON.parse(response);
@@ -10,6 +15,9 @@ $("document").ready(function() {
                 $("#response").text(JSON.stringify(JSON.parse(response.response_str), undefined, 2));
                 hljs.initHighlighting.called = false;
                 hljs.initHighlighting();
+                $('#send-request')
+                    .text("Отправить запрос")
+                    .attr("class", "btn btn-default");
             }
         });
     })
