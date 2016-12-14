648b407f27d3cbcf69aac366da851d550dcf0bc5
Twist-tracer <twist.tracer@gmail.com>
2016-12-14 11:14:25 +0300

    update

--- index.php
+++ index.php
@@ -1,20 +1,41 @@
 <?php
-/**
- * @var $data
- */
 require 'model.php';
 require 'query.php';
+
+/**
+ * Методы API
+	#/private/api/v2/json/accounts/current - информация об аккаунте
+
+	#/private/api/v2/json/leads/list - список сделок
+	#/private/api/v2/json/contacts/list - список контактов
+	#/private/api/v2/json/company/list - список компаний
+
+	#/private/api/v2/json/leads/set - добавить/обновить сделку
+	#/private/api/v2/json/contacts/set - добавить/обновить сделку
+	#/private/api/v2/json/company/set - добавить/обновить сделку
+
+	#/api/unsorted/add/?api_key=".$user["USER_HASH"]."&login=".$user["USER_LOGIN"]." - добавить в неразобранное
+	#/api/unsorted/list/?api_key=".$user["USER_HASH"]."&login=".$user["USER_LOGIN"]." - список неразобранного
+
+	#/private/api/v2/json/webhooks/list - список WebHooks
+	#/private/api/v2/json/webhooks/subscribe - добавить WebHook
+	#/private/api/v2/json/webhooks/unsubscribe - удалить WebHook
+
+	#/private/api/v2/json/contacts/links - Связи между сделками и контактами
+ *
+ * @var $data
+ */
 
 define('HOST', TRUE);
 define('NEWACC', FALSE);
 
 // Информация об аккаунте
 if(HOST) {
-    $domain = 'sbogdanov.amocrm.sass';
+    $domain = 'main3.amocrm2.saas';
 
     $user = [
         'USER_LOGIN' => 'twist.tracer@gmail.com',
-        'USER_HASH' => 'b2ef0353a07a3772d3a23c6ae0bb5668'
+        'USER_HASH' => '6b3d5b27d51c60a3c72af0c452250cd6'
     ];
 
     $subdomain = 'sbogdanov';
@@ -33,48 +54,35 @@ if(HOST) {
     }
 }
 
-/* Методы API */
-#/private/api/v2/json/accounts/current - информация об аккаунте
-
-#/api/unsorted/list/?api_key=".$user["USER_HASH"]."&login=".$user["USER_LOGIN"]." - список неразобранного
-#/private/api/v2/json/leads/list - список сделок
-#/private/api/v2/json/contacts/list - список контактов
-#/private/api/v2/json/company/list - список компаний
-#/private/api/v2/json/webhooks/list - список WebHooks
-
-#/api/unsorted/add/?api_key=".$user["USER_HASH"]."&login=".$user["USER_LOGIN"]." - добавить в неразобранное
-#/private/api/v2/json/leads/set - добавить/обновить сделку
-#/private/api/v2/json/contacts/set - добавить/обновить сделку
-#/private/api/v2/json/company/set - добавить/обновить сделку
-#/private/api/v2/json/webhooks/subscribe - добавить WebHook
-
-#/private/api/v2/json/webhooks/unsubscribe - удалить WebHook
-
-#/private/api/v2/json/contacts/links - Связи между сделками и контактами
-
-// Настройки запроса
-$link = "https://".$subdomain.".".$domain."/private/api/v2/json/accounts/current"; #ссылка на метод API
-$type = FALSE; #Тип запроса: FALSE(GET запрос, параметры нужно указать в url), CURLOPT_POST(Стандартный пост запрос), CURLOPT_CUSTOMREQUEST(Пост с телом запроса, например JSON)
+/** Настройки запроса **/
+/* Cсылка на метод API */
+$link = "https://".$subdomain.".".$domain."/private/api/v2/json/company/set";
+/* Тип запроса: */
+# FALSE (GET запрос, параметры нужно указать в url),
+# CURLOPT_POST (Стандартный пост запрос),
+# CURLOPT_CUSTOMREQUEST (Пост с телом запроса, например JSON)
+$type = 'CURLOPT_CUSTOMREQUEST';
 
 $log = FALSE;
 
-$request_str = ($type == 'CURLOPT_CUSTOMREQUEST')? file_get_contents('query.php', NULL, NULL, 20): $link;
+$headers = [
+	'Accept: application/json'
+];
+
+$request_str = ($type == 'CURLOPT_CUSTOMREQUEST' || $type == 'CURLOPT_POST') ? file_get_contents('query.php', NULL, NULL, 20) : $link;
 $status = "default";
 
 if(isset($_POST["query"]) && $_POST["query"] == "get_request_contents") {
-    echo $request_str;
-    exit;
+    die($request_str);
 }
 
 if(isset($_POST["query"]) && $_POST["query"] == "send_request") {
     if(!auth($user, $subdomain, $domain)) {
-        echo json_encode(["status" => "danger", "response_str" => "Авторизация не удалась!"]);
-        exit;
+        die(json_encode(["status" => "danger", "response_str" => "Авторизация не удалась!"]));
     } else {
-        $response = send_request($link, $data, $type, $log);
+        $response = send_request($link, $data, $type, $log, $headers); // get_entity_ids($entity, $link, $data, $type, $log, $headers)
         $status = ((int)$response["code"] != 200 && (int)$response["code"] != 201 && (int)$response["code"] != 204)? "danger" : "success";
-        echo json_encode(["status" => $status, "response_str" => $response["response_str"]]);
-        exit;
+        die(json_encode(["status" => $status, "response_str" => $response["response_str"]]));
     }
 }
 
--- query.php
+++ query.php
@@ -1,2 +1,26 @@
 <?php
 $data = [];
+function getRandomName() {
+	return strtoupper(mb_substr(md5(time() . mt_rand(0, 999)), 0, 8));
+}
+
+function getEntity() {
+	return [
+		[
+			'name' => getRandomName(),
+			'custom_fields' => [
+				[
+					'id' => 1357268,
+					'values' => [
+						[
+							'value' => 79554621645,
+							'enum' => 'MOB'
+						]
+					]
+				],
+			]
+		],
+	];
+}
+
+$data['request']['contacts']['add'] = getEntity();
